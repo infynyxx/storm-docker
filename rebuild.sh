5f3d8f91f2b98fc120d59b774f68ea0400409851
Prajwal Tuladhar <praj@infynyxx.com>
2015-02-11 17:49:53 -0500

    storm cluster is working now

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t="wurstmeister/storm" storm
-docker build -t="wurstmeister/storm-nimbus" storm-nimbus
-docker build -t="wurstmeister/storm-supervisor" storm-supervisor
-docker build -t="wurstmeister/storm-ui" storm-ui
+docker build -t="infynyxx/storm" storm
+docker build -t="infynyxx/storm-nimbus" storm-nimbus
+docker build -t="infynyxx/storm-supervisor" storm-supervisor
+docker build -t="infynyxx/storm-ui" storm-ui
